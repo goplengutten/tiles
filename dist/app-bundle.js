7e16deaccd5875982854e3c07d4cabfb91c1933a
goplengutten <goplengutten@gmail.com>
2022-07-16 21:45:18 +0200

    typescript + webpack

---
+++ dist/app-bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+
+// const canvas = document.getElementById("canvas");
+// console.log(canvas);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90aWxlcy8uL3NyYy9hcHAudHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG4vLyBjb25zdCBjYW52YXMgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcImNhbnZhc1wiKTtcbi8vIGNvbnNvbGUubG9nKGNhbnZhcyk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
